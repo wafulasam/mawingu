6923d850082f3353f64735a214bbaa287a168fcd
samuel barasa <sammie@Sammies-MacBook-Pro.local>
2021-11-06 22:08:10 +0300

    - app setup

---
+++ mawingu/mawinguApp.swift
@@ -0,0 +1,17 @@
+//
+//  mawinguApp.swift
+//  mawingu
+//
+//  Created by Sammie on 06/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct mawinguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
